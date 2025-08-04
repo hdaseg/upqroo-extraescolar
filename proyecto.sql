b9a63300a50ad916ff4e14367833fbc6b7560cdc
hdaseg <danniel2100@gmail.com>
2025-08-03 19:50:27 -0500

    creacion de base de datos

--- proyecto.sql
+++ proyecto.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `proyecto`
 --
-
+CREATE DATABASE IF NOT EXISTS proyecto CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE proyecto;
 -- --------------------------------------------------------
 
 --
